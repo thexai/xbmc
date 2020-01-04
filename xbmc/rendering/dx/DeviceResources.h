9869d9c9b207fb1db8c125c04108718daa71acf9
thexai <58434170+thexai@users.noreply.github.com>
2020-01-04 13:07:06 +0100

    Implemented swapchain re-creation to switch HDR/SDR without restart
    Implemented new base class GetOSHDRStatus() (thanks fuzzard)
    Implemented IsHDRDisplay() override
    Move windowing methods to CWinsystemWin32DX
    Crete new windowing methods for all needed deviceresources functions
    Use new windowing methods in all external files: DXVA, RenderBase, etc.
    Fixed small incorrectness DXVA HW tone mapping (clipped high luminance values)
    Remove unnecessary includes + cleanup

--- xbmc/rendering/dx/DeviceResources.h
+++ xbmc/rendering/dx/DeviceResources.h
@@ -76,6 +76,7 @@ namespace DX
     void ReleaseBackBuffer();
     void CreateBackBuffer();
     void ResizeBuffers();
+    void ReCreateSwapChain();
 
     bool SetFullScreen(bool fullscreen, RESOLUTION_INFO& res);
 
@@ -140,7 +141,7 @@ namespace DX
     Microsoft::WRL::ComPtr<ID3D11Device1> m_d3dDevice;
     Microsoft::WRL::ComPtr<ID3D11DeviceContext1> m_d3dContext;
     Microsoft::WRL::ComPtr<ID3D11DeviceContext1> m_deferrContext;
-    Microsoft::WRL::ComPtr<IDXGISwapChain1> m_swapChain;
+    Microsoft::WRL::ComPtr<IDXGISwapChain3> m_swapChain;
 #ifdef _DEBUG
     Microsoft::WRL::ComPtr<ID3D11Debug> m_d3dDebug;
 #endif
